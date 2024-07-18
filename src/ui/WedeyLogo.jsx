df094d5c3d06673df0e0cb38636945861a990999
Nana Kwame <desmondboateng483@gmail.com>
2024-07-18 01:36:15 +0000

    fixed some bugs

--- src/ui/WedeyLogo.jsx
+++ src/ui/WedeyLogo.jsx
@@ -1,7 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { cn } from "../utils/cn";
+import { useEffect } from "react";
 
 const WedeyLogo = ({ className }) => {
+  const pathName = useLocation();
+
+  useEffect(
+    function () {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    },
+    [pathName]
+  );
+
   return (
     <div className={cn("mb-0 ", className)}>
       <Link to="/" className="">
